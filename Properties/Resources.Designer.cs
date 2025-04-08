e64170c9d98053af0b1de569923cd5000317d6e6
Julia Isaksson <julia.jenny.isaksson@gmail.com>
2025-04-08 20:28:58 +0200

    Changed namespace to the correct one

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Assignment4VT25.Properties {
+namespace Assignment3VT25.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Assignment4VT25.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Assignment4VT25.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Assignment3VT25.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
